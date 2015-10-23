17ebc7c6b795559d436c1da4666969378a9e9fc4
Ru-T <rwajnberg@gmail.com>
2015-10-23 09:27:11 -0400

    Create Board class with test.

---
+++ board.rb
@@ -0,0 +1,6 @@
+require 'byebug'
+
+class Board
+
+
+end
---
+++ board_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'byebug'
+
+require './board.rb'
+
+class BoardTest < Minitest::Test
+
+  def test_board_class_exists
+    assert Board
+  end
+
+end  
