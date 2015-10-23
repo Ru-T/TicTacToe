0c7e751fc480a517aa0aae6a61a5dca833eec2ca
Ru-T <rwajnberg@gmail.com>
2015-10-23 09:45:02 -0400

    Add method for setting up game and asking for player names.

--- board.rb
+++ board.rb
@@ -2,16 +2,27 @@ require 'byebug'
 
 class Board
 
-  attr_reader :board
+  attr_reader :board, :player1, :player2
 
   def initialize
     @board = Array.new(9)
+    @player1 = player1
+    @player2 = player2
   end
 
   def display_board
     puts "| #{@board[0]} | #{@board[1]} | #{@board[2]} |"
     puts "| #{@board[3]} | #{@board[4]} | #{@board[5]} |"
     puts "| #{@board[6]} | #{@board[7]} | #{@board[8]} |"
+  end
+
+  def set_up_game
+    puts "Player 1, please enter your name."
+    name1 = gets.chomp
+    @player1 = name1
+    puts "Player 2, please enter your name."
+    name2 = gets.chomp
+    @player2 = name2
   end
 
 end
--- game.rb
+++ game.rb
@@ -2,3 +2,4 @@ require './board'
 
 board = Board.new
 board.display_board
+board.set_up_game
