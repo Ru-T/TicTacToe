f5011b823a2ddb5c0d88d345061dba82880295db
Ru-T <rwajnberg@gmail.com>
2015-10-25 21:50:02 -0400

    Move task list into sandbox.

--- sandbox.rb
+++ sandbox.rb
@@ -1,11 +1,39 @@
-  # ISSUES:
+# ISSUES:
 
-# MINIMAX: recursive method - is this doing what I think it's doing?
 # middle_score method: how do I define scenario? What about when @moves is empty?
-# HELP WITH TESTS!!!!
-# private: easier way than putting private before every method?
-# play_game method: can this be improved?
+# recursive method - is this doing what I think it's doing?
+# TESTS!!!!
+# Refactor in multiple classes
 # winner method: do I need @win to pass back who won?
 # winner method: do I need both @win and @score??
+# play_game method: can this be improved?
 # ComputerPlayer doesn't have a name
-#Refactor x && y && part of if statement - big if/else and mini inside
+# Refactor x && y && part of if statement - big if/else and mini inside
+
+# Task List
+#
+# [ X ] Create empty tic tac toe board
+# [ X ] User should be asked for a location (A1, A2 etc.)
+# [ X ] When user enters location, X is marked not he board.
+# [ X ] If user enters invalid entry, reprimanded and try again.
+# [ X ] User inputs valid locations until the board is full, then the game is over.
+# [ X ] Create concept of position and tell user if location is already occupied.
+# [ X ] Allow two human players to play.
+# [ X ] The first marks the board with “X”, the second marks the board with “O”.
+# [ X ] Any invalids of duplicates should alert the player but stay on their turn until they enter a valid location.
+# [ X ] Allow a player to win.
+# [ X ] If a player wins, end the game and report who won.
+# [ X ] Create the concept of a computer player.
+# [ X ] If the computer is playing, choose a random position. (dumb computer)
+# [ X ] Either computer or human can win.
+# Unbeatable
+#
+# [ X ] Create concept of possible remaining moves.
+# [ X ] Create concept of score when win, lose, draw.
+# [   ] Create decision tree from each remaining move.
+# [   ] Computer chooses move with min score (same index.)
+# [   ] Optimize - stop minimax when a move returns first loss.
+#
+# BUGS
+#
+# [   ] Computer player does not have a name
